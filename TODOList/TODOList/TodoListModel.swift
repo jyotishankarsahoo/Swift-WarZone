d1f9bbc3ad236607339d8e74894c5e8ebe5bc805
jyotishankar sahoo <jyotishankar.sahoo@exilant.com>
2016-07-26 16:25:35 +0530

    TODOList Application swift

---
+++ TODOList/TODOList/TodoListModel.swift
@@ -0,0 +1,13 @@
+//
+//  TodoListModel.swift
+//  TODOList
+//
+//  Created by jyotishankar sahoo on 26/07/16.
+//  Copyright © 2016 jyotishankar sahoo. All rights reserved.
+//
+
+import Cocoa
+
+class TodoListModel: NSObject {
+
+}
