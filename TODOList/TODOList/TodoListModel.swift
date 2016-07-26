bc1c57e0001b081b31e7814aaa01039cd0a9d8cd
jyotishankar sahoo <jyotishankar.sahoo@exilant.com>
2016-07-26 16:26:31 +0530

    Added Caching mechanism

--- TODOList/TODOList/TodoListModel.swift
+++ TODOList/TODOList/TodoListModel.swift
@@ -6,8 +6,83 @@
 //  Copyright © 2016 jyotishankar sahoo. All rights reserved.
 //
 
-import Cocoa
+import Foundation
+import UIKit
 
 class TodoListModel: NSObject {
 
+    var filePath : NSURL = {
+        let documentDirectoryURLs = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let directoryURL = documentDirectoryURLs.first!
+        return NSURL(fileURLWithPath: directoryURL.stringByAppendingString("todolist.items"))
+    }()
+    
+    var items = [String]()
+    
+    
+    override init() {
+        super.init()
+        loadFromFile()
+    }
+    
+    func addItem(item : String)  {
+        items.append(item)
+        self.writeToFile()
+    }
+    
+    func removeItemAtIndex(index : Int){
+        items.removeAtIndex(index)
+        self.writeToFile()
+    }
+    
+    func writeToFile(){
+        let itemArray = items as NSArray
+        if !itemArray.writeToURL(filePath, atomically: true){
+            print("Unable to write to file")
+        }
+    }
+    
+    func loadFromFile(){
+        if let itemsArray = NSArray(contentsOfURL: filePath) as? [String]{
+            items = itemsArray
+        }
+    }
+}
+
+extension TodoListModel : UITableViewDataSource{
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("todoCell", forIndexPath: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    // Gold Challenge
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool{
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == .Delete{
+            tableView.beginUpdates()
+            self.removeItemAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+            tableView.endUpdates()
+        }
+    }
+    
+//    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool{
+//            return true
+//    }
+//    
+//    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath){
+//        let itemToMove = items[sourceIndexPath.row]
+//        items.removeAtIndex(sourceIndexPath.row)
+//        items.insert(itemToMove, atIndex: destinationIndexPath.row)
+//    }
+    
 }
--- TODOList/TODOList/ViewController.swift
+++ TODOList/TODOList/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let todoList = TodoListModel()
+    
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func insertItem(sender: AnyObject) {
+        //Bronze Challenge
+        guard let enteredText = self.itemTextField.text where !enteredText.isEmpty else{
+            return
+        }
+        
+        self.itemTextField.text = nil
+        self.todoList.addItem(enteredText)
+        self.tableView.reloadData()
+    }
+    
+//    @IBAction func editAction(sender: UIBarButtonItem) {
+//        if self.tableView.editing{
+//            self.tableView.setEditing(false, animated: true)
+//            sender.title  = "Done"
+//            //sender.style = UIBarButtonItemStyle.Plain
+//        }else{
+//            self.tableView.setEditing(true, animated: true)
+//            sender.title = "Edit"
+//            //sender.style = UIBarButtonItemStyle.Done
+//        }
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "todoCell")
+        self.tableView.dataSource = todoList
     }
 
     override func didReceiveMemoryWarning() {
