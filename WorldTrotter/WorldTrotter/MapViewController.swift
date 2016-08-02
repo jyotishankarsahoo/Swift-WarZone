ff87a210864e73f625b84b2463f3abfac0cdb542
jyotishankar sahoo <jyotishankar.sahoo@exilant.com>
2016-08-02 12:55:58 +0530

    Map view added Programmatically with constraints

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -8,11 +8,57 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class MapViewController : UIViewController {
+    
+    var mapView : MKMapView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Map View loaded")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        //create mapview
+        mapView = MKMapView()
+        //Set it to the viewcontroller's view
+        view = mapView
+        
+        //create segment view
+        let segmentView = UISegmentedControl(items: ["Standard","Hybrid","Satellite"])
+        segmentView.backgroundColor = UIColor.whiteColor()
+        segmentView.selectedSegmentIndex = 0
+        segmentView.translatesAutoresizingMaskIntoConstraints = false
+        //Add target
+        segmentView.addTarget(self, action: #selector(MapViewController.updateMapStyle(_:)), forControlEvents: .ValueChanged)
+        //add to view
+        view.addSubview(segmentView)
+        
+        //Define Anchors
+        let topConstraint = segmentView.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
+        let trailingConstraint = segmentView.trailingAnchor.constraintEqualToAnchor(view.layoutMarginsGuide.trailingAnchor)
+        let leadingConstraint = segmentView.leadingAnchor.constraintEqualToAnchor(view.layoutMarginsGuide.leadingAnchor)
+        
+        //Active Constraints
+        topConstraint.active = true
+        trailingConstraint.active = true
+        leadingConstraint.active = true
+        
+    }
+    
+    func updateMapStyle (segControl : UISegmentedControl){
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            self.mapView?.mapType = .Standard
+        case 1:
+            self.mapView?.mapType = .Hybrid
+        case 2:
+            self.mapView?.mapType = .Satellite
+        default:
+            fatalError("Invalide selection index")
+        }
     }
 }
