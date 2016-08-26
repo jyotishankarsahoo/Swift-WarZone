515c895be0d28728a5e7a5ba5842229d8ba0b962
jyotishankar sahoo <jyotishankar.sahoo@exilant.com>
2016-08-26 19:02:51 +0530

    Updated with Add, Edit and delete functionality

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -13,19 +13,34 @@ class ItemStore {
     var allItems = [Item]()
     var sortedItems = [Item]()
     
-    init(){
-        for _ in 0..<5 {
-            self.createItem()
-        }
-        sortedItems = allItems.sort({ (x, y) -> Bool in
-          return  x.valueInDollar < y.valueInDollar
-        })
-        
-    }
+//    init(){
+//        for _ in 0..<5 {
+//            self.createItem()
+//        }
+//        sortedItems = allItems.sort({ (x, y) -> Bool in
+//          return  x.valueInDollar < y.valueInDollar
+//        })
+//        
+//    }
     
     func createItem() -> Item {
         let newItem = Item(random : true)
         allItems.append(newItem)
         return newItem
+    }
+    
+    func removeItem(item : Item)  {
+        if let index = allItems.indexOf(item){
+            allItems.removeAtIndex(index)
+        }
+    }
+    
+    func moveItem(fromIndex : Int , toIndex : Int)  {
+        if fromIndex == toIndex {
+            return
+        }
+        let itemToBeMoved = allItems[fromIndex]
+        allItems.removeAtIndex(fromIndex)
+        allItems.insert(itemToBeMoved, atIndex: toIndex)
     }
 }
--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -10,8 +10,26 @@ import Foundation
 import UIKit
 class ItemsViewController: UITableViewController  {
     var itemStore : ItemStore!
-    var itemLessThanFive : Array<Item>?
-    var itemMoreThanFive : Array<Item>?
+    //var itemLessThanFive : Array<Item>?
+    //var itemMoreThanFive : Array<Item>?
+    
+    @IBAction func toggleEditMode(sender : UIButton){
+        if editing {
+            sender.setTitle("Edit", forState: .Normal)
+            setEditing(false, animated: true)
+        }else{
+            sender.setTitle("Done", forState: .Normal)
+            setEditing(true, animated: true)
+        }
+    }
+    
+    @IBAction func addItem(sender : UIButton){
+        let newItem = itemStore.createItem()
+        if let index = itemStore.allItems.indexOf(newItem){
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
 
     //MARK: - Viewcontroller Life cycle
     override func viewDidLoad() {
@@ -25,38 +43,44 @@ class ItemsViewController: UITableViewController  {
     //MARK: - Data source delegate method overriden
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //Bronze Challenge
-        if section == 0 {
-            itemLessThanFive =  itemStore.sortedItems.filter({ (x) -> Bool in
-                return x.valueInDollar <= 50
-            })
-            return itemLessThanFive!.count
-        }else{
-            itemMoreThanFive =  itemStore.sortedItems.filter({ (x) -> Bool in
-                return x.valueInDollar > 50
-            })
-            return itemMoreThanFive!.count
-        }
+//        if section == 0 {
+//            itemLessThanFive =  itemStore.allItems.filter({ (x) -> Bool in
+//                return x.valueInDollar <= 50
+//            })
+//            return itemLessThanFive!.count
+//        }else{
+//            itemMoreThanFive =  itemStore.allItems.filter({ (x) -> Bool in
+//                return x.valueInDollar > 50
+//            })
+//            return itemMoreThanFive!.count
+//        }
+        return itemStore.allItems.count
 
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .Value1, reuseIdentifier: "cell")
+        //GOLD Challenge
        cell.textLabel?.font = cell.textLabel?.font.fontWithSize(20)
-        if indexPath.section == 0 {
-            cell.textLabel?.text = itemLessThanFive![indexPath.row].name
-            cell.detailTextLabel?.text = String (itemLessThanFive![indexPath.row].valueInDollar)
-        }else{
-            cell.textLabel?.text = itemMoreThanFive![indexPath.row].name
-            cell.detailTextLabel?.text = String (itemMoreThanFive![indexPath.row].valueInDollar)
-        }
+//        if indexPath.section == 0 {
+//            cell.textLabel?.text = itemLessThanFive![indexPath.row].name
+//            cell.detailTextLabel?.text = String (itemLessThanFive![indexPath.row].valueInDollar)
+//        }else{
+//            cell.textLabel?.text = itemMoreThanFive![indexPath.row].name
+//            cell.detailTextLabel?.text = String (itemMoreThanFive![indexPath.row].valueInDollar)
+//        }
+        
+        cell.textLabel?.text = itemStore.allItems[indexPath.row].name
+        cell.detailTextLabel?.text = String (itemStore.allItems[indexPath.row].valueInDollar)
+
         return cell
     }
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int{
-        return 2
+        return 1
     }
     
     //Silver Challenge
     override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String?{
-        if section == 1 {
+        if section == 0 {
             return "No More Items"
         }
         return ""
@@ -67,6 +91,14 @@ class ItemsViewController: UITableViewController  {
         return 60
     }
 
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == .Delete {
+            itemStore.removeItem(itemStore.allItems[indexPath.row])
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
     
-    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath){
+        itemStore.moveItem(sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
 }
