e5ba58d5319cc1eb6ab022ac127a844d8e3e74b4
jyotishankar sahoo <jyotishankar.sahoo@exilant.com>
2017-01-16 11:55:49 +0530

    Multiple touch support added

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -13,6 +13,7 @@ class DrawView: UIView {
     
     var currentLine : Line?
     var finishedLine = [Line]()
+    var currentLines = [NSValue : Line]()
     
     func strokeLine(line : Line){
         let path = UIBezierPath()
@@ -24,6 +25,8 @@ class DrawView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
+        // For Single touch
+        /*
         UIColor.black.setStroke()
         for line in finishedLine {
             self.strokeLine(line: line)
@@ -32,23 +35,58 @@ class DrawView: UIView {
             UIColor.red.setStroke()
             self.strokeLine(line: line)
         }
+         */
+        // Multiple touches
+        UIColor.black.setStroke()
+        for line in finishedLine {
+            self.strokeLine(line: line)
+        }
+        UIColor.red.setStroke()
+        for ( _,line) in currentLines{
+            self.strokeLine(line: line)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // For Single touch
+        /*
         let touch = touches.first!
         let location = touch.location(in: self)
         currentLine = Line(begin: location, end: location)
         setNeedsDisplay()
+         */
+        
+        // Multiple touches
+        for touch in touches {
+            let location = touch.location(in: self)
+            let newLine = Line(begin: location, end: location)
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key] = newLine
+        }
+        setNeedsDisplay()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // For Single touch
+        /*
         let touch = touches.first!
         let location = touch.location(in: self)
         currentLine?.end = location
         setNeedsDisplay()
+         */
+        
+        // Multiple touches
+        for touch in touches {
+            let location = touch.location(in: self)
+            let key = NSValue(nonretainedObject : touch)
+            currentLines[key]?.end = location
+        }
+        setNeedsDisplay()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //For Single Touch
+        /*
         if var line = currentLine{
             let touch = touches.first!
             let location = touch.location(in: self)
@@ -57,5 +95,23 @@ class DrawView: UIView {
         }
         currentLine = nil
         setNeedsDisplay()
+         */
+        
+        //For multiple touches
+        for touch in touches {
+            let key = NSValue(nonretainedObject : touch)
+            if var line = currentLines[key]{
+                line.end = touch.location(in: self)
+                finishedLine.append(line)
+                currentLines.removeValue(forKey: key)
+            }
+            setNeedsDisplay()
+        }
     }
+ 
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        currentLines.removeAll()
+        setNeedsDisplay()
+    }
+        
 }
